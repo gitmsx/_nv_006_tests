2cafe96c3e8e5661a8603cd8dc85d7aee3483382
gitmsx <gitmsx@mail.ru>
2022-11-21 18:30:19 +0300

    Saved on 21.11.2022-18:30:19,01

--- Assets/Scripts/InitFieldChess.cs
+++ Assets/Scripts/InitFieldChess.cs
@@ -1,83 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-using Color = UnityEngine.Color;
 
 
 
 public class InitFieldChess : MonoBehaviour
 {
 
-    private GameObject BoxClone;
+
     public GameObject BulletPF;
     public GameObject BulletPF2;
-    //    public GameObject BulletPF3;
-    public float scale_pf;
-
-    // Start is called before the first frame update
-    public Texture2D sourceTex;
-    public Rect sourceRect;
+    private GameObject BoxClone;
 
     void Start()
     {
-        scale_pf = _global.Global_Scale;
-        Fire();
-
+        ChessBoard();
     }
 
 
 
-
-    // Update is called once per frame
-    void Update()
+    void ChessBoard()
     {
 
-    }
+        float scale_pf = _global.Global_Scale;
+        GameObject[] ChessTmp = new GameObject[2];
 
-
-    void Fire()
-    {
-
-        int x = Mathf.FloorToInt(sourceRect.x);
-        int y = Mathf.FloorToInt(sourceRect.y);
-        int width = Mathf.FloorToInt(sourceRect.width);
-        int height = Mathf.FloorToInt(sourceRect.height);
-
-
-
+        ChessTmp[0] = BulletPF;
+        ChessTmp[1] = BulletPF2;
 
         for (int i = 0; i < 20; i++)
             for (int j = 0; j < 20; j++)
-
             {
                 Vector3 NewPos = new Vector3(scale_pf * i, 0, scale_pf * j);
-                if ((i + j) % 2== 1)
-                {
-
-                    BoxClone = Instantiate(BulletPF, NewPos, transform.rotation);
-
-                }
-                
-
-                else
-
-                {
-                    BoxClone = Instantiate(BulletPF2, NewPos, transform.rotation);
-                }
-
-
+                BoxClone = Instantiate(ChessTmp[(i + j) % 2], NewPos, transform.rotation);
+                BoxClone.transform.localScale = new Vector3(2, 0.1f, 2);
             }
-
-
-
-
-
-
-
-
-
-
 
     }
 
